ac93dd7eb3d0f09c2e96e64b0ce6c647248ade18
Sunday Destiny <destinyjuniorsdj@gmail.com>
2021-01-16 12:33:17 +0100

    mobile view almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "14": "3.5rem",
+        "18": "4.5rem",
+        "68": "17.2rem",
+        "82": "21.8rem",
+        "98": "33rem"
+      }
+    },
   },
   variants: {
     extend: {},
